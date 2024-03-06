1d451e92d00a3b7cecb0b8ad8e6c85270689ca86
breadleaf <bkhnapa@gmail.com>
2024-03-06 11:33:25 -0700

    update gitignore and debug file path

--- start.sh
+++ start.sh
@@ -3,20 +3,20 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ $# -ne 1 ]; then
-    echo "Usage: ./start.sh [1 | 2]" > ~/dunst-mute-applet.log 2>&1
+    echo "Usage: ./start.sh [1 | 2]" > $DIR/dunst-mute-applet.log 2>&1
     exit 1
 fi
 
 # if ./venv/ does not exist in DIR, create it
 if [ ! -d "$DIR/venv/" ]; then
-    python3 -m venv $DIR/venv > ~/dunst-mute-applet.log 2>&1
+    python3 -m venv $DIR/venv > $DIR/dunst-mute-applet.log 2>&1
 fi
 
 # activate the virtual environment
 source $DIR/venv/bin/activate
 
 # install the required packages
-pip install -r $DIR/requirements.txt > ~/dunst-mute-applet.log 2>&1
+pip install -r $DIR/requirements.txt > $DIR/dunst-mute-applet.log 2>&1
 
 # run the application
-python3 $DIR/main.py $1 > ~/dunst-mute-applet.log 2>&1
+python3 $DIR/main.py $1 > $DIR/dunst-mute-applet.log 2>&1
